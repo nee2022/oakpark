dcad6eb269738fd1e7ae5868a8e412bd398b5edc
zhangyihua <zhangyihua_2020@qq.com>
2021-03-03 16:26:08 +0800

    初始化

--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,31 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
 // import login from '../views/login.vue'
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
-  // {
-  //   path: '/',
-  //   name: 'login',
-  //   component: login
-  // },
-  {
-    path: '/',
-    name: 'login',
-    component: () => import('../views/login.vue')
-  }
-]
+    // {
+    //   path: '/',
+    //   name: 'login',
+    //   component: login
+    // },
+    // {
+    //     path: '/',
+    //     name: 'login',
+    //     component: () => import('../views/login.vue'),
+    // },
+    {
+        path: '/',
+        name: 'login',
+        component: () => import('../views/paymentSuccess.vue'),
+    },
+];
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
-})
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes,
+});
 
-export default router
+export default router;
