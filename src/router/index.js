2ef5b220be80e956ca40b9f1c453d1e5cd9c5a89
zhangyihua <zhangyihua_2020@qq.com>
2021-03-04 10:26:35 +0800

    初始化我的项目

--- src/router/index.js
+++ src/router/index.js
@@ -1,32 +1,37 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
 // import login from '../views/login.vue'
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
-  // {
-  //   path: '/',
-  //   name: 'login',
-  //   component: login
-  // },
-  {
-    path: '/',
-    name: 'saomajiaofei',
-    component: () => import('../views/saomajiaofei.vue')
-  },
-  {
-    path: '/login',
-    name: 'login',
-    component: () => import('../views/login.vue')
-  },
- 
-]
+    // {
+    //   path: '/',
+    //   name: 'login',
+    //   component: login
+    // },
+    {
+        path: '/',
+        name: 'saomajiaofei',
+        component: () => import('../views/saomajiaofei.vue'),
+    },
+    {
+        path: '/login',
+        name: 'login',
+        component: () => import('../views/login.vue'),
+    },
+
+    {
+        path: '/paymentSuccess',
+        name: 'paymentSuccess',
+        component: () => import('../views/paymentSuccess.vue'),
+    },
+];
 
 const router = new VueRouter({
-  mode: 'hash',
-  base: process.env.BASE_URL,
-  routes
-})
+    mode: 'hash',
+    base: process.env.BASE_URL,
+    routes,
+});
 
-export default router
+export default router;
